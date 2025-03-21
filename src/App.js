8d14eef47febbcb6a6e22c8f606dfe3ab7eecb05
Bhavith <bhaviths20@gmail.com>
2025-03-21 23:59:44 +0530

    Added 3D effect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import "./App.css"
+
 function App() {
   return (
     <div className="App">
+      <div className="face" id="front">front</div>
+      <div className="face" id="back">back</div>
+      <div className="face" id="right">right</div>
+      <div className="face" id="left">left</div>
     </div>
   );
 }
